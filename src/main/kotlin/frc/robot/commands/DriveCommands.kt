c2a86024f35ab68e10c5a859aec7bdd65978f99a
aaditsangvikar <aadit.sangvikar@gmail.com>
2023-02-01 20:03:10 -0700

    readability, unlimitedDrive command

--- src/main/kotlin/frc/robot/commands/DriveCommands.kt
+++ src/main/kotlin/frc/robot/commands/DriveCommands.kt
@@ -6,10 +6,10 @@ import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.DrivetrainConstants
 import frc.robot.subsystems.SwerveSubsystem
 
-class StandardDrive(val swerve: SwerveSubsystem, val forward: () -> Double, val sideways: () -> Double, val radians: () -> Double, val fieldRelative: Boolean) : CommandBase() {
+class StandardDrive(val swervesubsystem: SwerveSubsystem, val forward: () -> Double, val sideways: () -> Double, val radians: () -> Double, val fieldRelative: Boolean) : CommandBase() {
     var radianLimiter = SlewRateLimiter(1.0, 1.0, 0.0)
     init {
-        addRequirements(swerve)
+        addRequirements(swervesubsystem)
     }
 
     override fun execute() {
@@ -17,10 +17,29 @@ class StandardDrive(val swerve: SwerveSubsystem, val forward: () -> Double, val 
         val sidewaysDesired = MathUtil.applyDeadband(sideways(), 0.06)
         val radiansDesired = radianLimiter.calculate(MathUtil.applyDeadband(radians(), 0.06))
 
-        swerve.drive(forwardDesired, sidewaysDesired, radiansDesired, fieldRelative)
+        swervesubsystem.drive(forwardDesired, sidewaysDesired, radiansDesired, fieldRelative)
     }
 
     override fun end(interrupted: Boolean) {
-        swerve.drive(0.0,0.0,0.0,false)
+        swervesubsystem.drive(0.0,0.0,0.0,true)
+    }
+}
+
+class UnlimitedDrive(val swervesubsystem: SwerveSubsystem, val forward: () -> Double, val sideways: () -> Double, val radians: () -> Double, val fieldRelative: Boolean) : CommandBase() {
+
+    init {
+        addRequirements(swervesubsystem)
+    }
+
+    override fun execute() {
+        val forwardDesired = MathUtil.applyDeadband(forward(), 0.06)
+        val sidewaysDesired = MathUtil.applyDeadband(sideways(), 0.06)
+        val radiansDesired = MathUtil.applyDeadband(radians(), 0.06)
+
+        swervesubsystem.drive(forwardDesired, sidewaysDesired, radiansDesired, fieldRelative)
+    }
+
+    override fun end(interrupted: Boolean) {
+        swervesubsystem.drive(0.0,0.0,0.0,true)
     }
 }
