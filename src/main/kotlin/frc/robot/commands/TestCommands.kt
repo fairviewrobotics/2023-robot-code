93c481116891e3c9a36a5059eecbf09f5e95de75
Michael Brennan <mebrennan01@bvsd.org>
2023-02-22 10:00:06 -0700

    Arm refactor and arm constant configuration
    additional test commands

--- src/main/kotlin/frc/robot/commands/TestCommands.kt
+++ src/main/kotlin/frc/robot/commands/TestCommands.kt
@@ -2,8 +2,10 @@ package frc.robot.commands
 
 import com.revrobotics.SparkMaxAbsoluteEncoder
 import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj2.command.CommandBase
+import frc.robot.constants.ArmConstants
 import frc.robot.constants.DrivetrainConstants
 import frc.robot.subsystems.SparkMaxSubsystem
 
@@ -45,5 +47,88 @@ class RezeroingCommand(val quad: String, val controller: XboxController, val tur
         }
 
         absoluteEncoderTopic.set(absoluteEncoder.position)
+    }
+}
+
+/**
+ * For quickly testing a single motor
+ */
+class QuickSpin(val controller: XboxController, val motor: SparkMaxSubsystem, val currentLimit: Int): CommandBase() {
+    val voltageTopic = NetworkTableInstance.getDefault().getTable("QuickSpin").getDoubleTopic("Voltage").publish()
+    val currentTopic = NetworkTableInstance.getDefault().getTable("QuickSpin").getDoubleTopic("Current").publish()
+    val powerTopic = NetworkTableInstance.getDefault().getTable("QuickSpin").getDoubleTopic("Power").publish()
+    init {
+        addRequirements(motor)
+        motor.x.setSmartCurrentLimit(currentLimit)
+        motor.x.burnFlash()
+    }
+
+    override fun execute() {
+        motor.x.setVoltage(controller.leftY * 12.0)
+        voltageTopic.set(motor.x.appliedOutput / motor.x.outputCurrent)
+        currentTopic.set(motor.x.outputCurrent)
+        powerTopic.set(motor.x.appliedOutput)
+    }
+
+    override fun end(interrupted: Boolean) {
+        motor.x.setVoltage(0.0)
+    }
+}
+
+class AbsoluteEncoderSetting(val controller: XboxController, val motor: SparkMaxSubsystem, val currentLimit: Int) : CommandBase() {
+    init {
+        addRequirements(motor)
+        motor.x.setSmartCurrentLimit(currentLimit)
+        motor.x.burnFlash()
+        motor.x.encoder.positionConversionFactor = 1.0
+        motor.x.encoder.velocityConversionFactor = 1.0
+    }
+
+    val positionRaw = NetworkTableInstance.getDefault().getTable("EncoderSetting").getDoubleTopic("RawPosition").publish()
+    val positionTrue = NetworkTableInstance.getDefault().getTable("EncoderSetting").getDoubleTopic("TruePosition").publish()
+
+    val position get() = 1.0
+    override fun execute() {
+        motor.x.setVoltage((controller.leftTriggerAxis - controller.rightTriggerAxis) * 12.0)
+
+        positionRaw.set(motor.x.encoder.position)
+        positionTrue.set(position)
+    }
+}
+
+class ElevatorSetting(val motor: SparkMaxSubsystem, val controller: XboxController, val bottomBreaker: DigitalInput, val topBreaker: DigitalInput) : CommandBase() {
+    init {
+        addRequirements(motor)
+        motor.x.encoder.positionConversionFactor = ArmConstants.elevatorEncoderPositionConversionFactor
+        motor.x.encoder.velocityConversionFactor = ArmConstants.elevatorEncoderVelocityMultiplier
+
+    }
+
+    object Telemetry {
+        val position = NetworkTableInstance.getDefault().getTable("ElevatorSetting").getDoubleTopic("Position").publish()
+        val topBreaker = NetworkTableInstance.getDefault().getTable("ElevatorSetting").getBooleanTopic("Top Hit").publish()
+        val bottomBreaker = NetworkTableInstance.getDefault().getTable("ElevatorSetting").getBooleanTopic("Bottom Hit").publish()
+    }
+
+    val topHit get() = !topBreaker.get()
+    val bottomHit get() = !bottomBreaker.get()
+
+    override fun execute() {
+        var elevatorVoltage = (controller.leftTriggerAxis - controller.rightTriggerAxis) * 12.0
+
+        if (topHit && elevatorVoltage > 0.0) {
+            elevatorVoltage = 0.0
+            //motor.x.encoder.position = ArmConstants.elevatorMaxHeight
+        }
+
+        if (bottomHit && elevatorVoltage < 0.0) {
+            elevatorVoltage = 0.0
+            motor.x.encoder.position = ArmConstants.elevatorMinHeight
+        }
+
+        motor.x.setVoltage(elevatorVoltage)
+        Telemetry.position.set(motor.x.encoder.position)
+        Telemetry.topBreaker.set(topHit)
+        Telemetry.bottomBreaker.set(bottomHit)
     }
 }
