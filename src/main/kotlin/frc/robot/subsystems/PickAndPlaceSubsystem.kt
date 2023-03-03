024b57c21fd994dd83f19678c5421734eb16f4df
Michael Brennan <mebrennan01@bvsd.org>
2023-03-02 18:53:49 -0700

    monday

--- src/main/kotlin/frc/robot/subsystems/PickAndPlaceSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/PickAndPlaceSubsystem.kt
@@ -3,6 +3,7 @@ package frc.robot.subsystems
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import com.revrobotics.SparkMaxAbsoluteEncoder
+import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -16,6 +17,7 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
                             intakeMotorTwoId: Int,
                             topBreakerID: Int,
                             bottomBreakerID: Int) : SubsystemBase(){
+
     val elevatorMotor = CANSparkMax(elevatorMotorId, CANSparkMaxLowLevel.MotorType.kBrushless)
     val elbowMotor = CANSparkMax(elbowMotorId, CANSparkMaxLowLevel.MotorType.kBrushless)
     val wristMotor = CANSparkMax(wristMotorId, CANSparkMaxLowLevel.MotorType.kBrushless)
@@ -51,16 +53,20 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
         val bottomHit = NetworkTableInstance.getDefault().getTable("Arm").getBooleanTopic("BottomHit").publish()
         val topHit = NetworkTableInstance.getDefault().getTable("Arm").getBooleanTopic("TopHit").publish()
         val wristPosition = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("WristPosition").publish()
-        val wristVoltageNT = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("WristVoltage").publish()
-        val elbowVoltageNT = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("ElbowVolts").publish()
-        val elevatorVoltageNT = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("ElevatorVolts").publish()
-        val intakeVoltageNT = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("IntakeVolts").publish()
+        val wristVoltage = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("WristVoltage").publish()
+        val elbowVoltage = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("ElbowVolts").publish()
+        val intakeVoltage = NetworkTableInstance.getDefault().getTable("Arm").getDoubleTopic("IntakeVolts").publish()
+
+        val elevatorZeroed =  NetworkTableInstance.getDefault().getTable("Arm").getBooleanTopic("ElevatorZeroed").publish()
 
     }
 
 
     init {
         //intake
+        elbowMotor.setSmartCurrentLimit(20)
+        wristMotor.setSmartCurrentLimit(20)
+        elbowMotor.inverted = ArmConstants.elbowMotorInverted
         wristMotor.idleMode = CANSparkMax.IdleMode.kBrake
         intakeOneMotor.idleMode = CANSparkMax.IdleMode.kBrake
         intakeTwoMotor.idleMode = CANSparkMax.IdleMode.kBrake
@@ -80,7 +86,7 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
         wristMotor.burnFlash()
         //everything else:
         // TODO: Elevator conversion factors have been tuned, but the elbow conversion factors have not.
-        elbowEncoder.positionConversionFactor = 2.0 * Math.PI / 60.0
+        elbowEncoder.positionConversionFactor = 2.0 * Math.PI
         elbowEncoder.velocityConversionFactor = 2.0 * Math.PI / 60.0
 
         elevatorEncoder.positionConversionFactor = ArmConstants.elevatorEncoderPositionConversionFactor
@@ -91,16 +97,18 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
         // TODO: wristEncoder position and velocity conversion factor
     }
 
-    val elbowPositionRadians get() = elbowEncoder.position + ArmConstants.elbowEncoderPosOffset
+    val elbowPositionRadians get() = Rotation2d(elbowEncoder.position).plus(Rotation2d(absoluteWristPosition)).minus(Rotation2d(05.880)).minus(
+        Rotation2d(-1.25)
+    ).radians
     val elevatorPositionMeters get() = elevatorEncoder.position
 
     //intake
-    val relativeWristPosition get() = 2.0
+    val absoluteWristPosition get() = Rotation2d(wristEncoder.position).minus(Rotation2d(0.900)).radians
 
     /** This is the pitch with taking consideration to the position of the elbow.
      * 0 degrees means the intake is parallel to the ground.
      */
-    val absoluteWristPosition get() = 2.0
+    val relativeWristPosition get() = 2.0
 
     /** This value sets the voltage for the intake motors. Positive value will spin the wheels inward
      * and pick objects up.
@@ -130,7 +138,9 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
     var elevatorVoltage = 0.0
         set(x: Double) {
             field = x
-            if (bottomHit && x < 0.0) {
+            if (!elevatorZeroed) {
+                elevatorMotor.setVoltage(-3.0)
+            } else if (bottomHit && x < 0.0) {
                 elevatorMotor.setVoltage(0.0)
             } else if (topHit && x > 0.0) {
                 elevatorMotor.setVoltage(0.0)
@@ -141,10 +151,14 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
 
     override fun periodic() {
         super.periodic()
+        if (!elevatorZeroed) {
+            elevatorMotor.setVoltage(-3.0)
+        }
         // These are the voltages we set and will be sent to the elevator and elbow.
         if(bottomHit)
         {
             elevatorEncoder.position = ArmConstants.elevatorMinHeight
+            elevatorZeroed = true
         }
         else if(topHit)
         {
@@ -152,19 +166,19 @@ class PickAndPlaceSubsystem(elevatorMotorId : Int,
         }
 
         // Telemetry setting.
-        //Telemetry.elevatorVoltage.set(elevatorVoltage)
-        Telemetry.elbowPosition.set(elbowEncoder.position)
+        Telemetry.elevatorZeroed.set(elevatorZeroed)
+        Telemetry.elbowPosition.set(elbowPositionRadians)
         Telemetry.elevatorPosition.set(elevatorEncoder.position)
-        Telemetry.wristPosition.set(wristEncoder.position)
+        Telemetry.wristPosition.set(absoluteWristPosition)
         Telemetry.elbowVelocity.set(elbowEncoder.velocity)
         Telemetry.topHit.set(topHit)
         Telemetry.bottomHit.set(bottomHit)
-        Telemetry.wristVoltageNT.set(wristVoltage)
-        Telemetry.elbowVoltageNT.set(elbowVoltage)
-        Telemetry.elevatorVoltageNT.set(elevatorVoltage)
-        Telemetry.intakeVoltageNT.set(intakesVoltage) //petition to call intake "intakes"
-        //Telemetry.elevatorPosition.set(elevatorPositionMeters)
-        //Telemetry.elevatorVelocity.set(elevatorEncoder.velocity)
+        Telemetry.wristVoltage.set(wristVoltage)
+        Telemetry.elbowVoltage.set(elbowVoltage)
+        Telemetry.elevatorVoltage.set(elevatorVoltage)
+        Telemetry.intakeVoltage.set(intakesVoltage)
+        Telemetry.elevatorPosition.set(elevatorPositionMeters)
+        Telemetry.elevatorVelocity.set(elevatorEncoder.velocity)
     }
 
 }
