bb509fbf7e39a97a232569c530d7b44209502e13
zaneal <102438595+zaneal@users.noreply.github.com>
2023-03-14 00:33:00 -0600

    Changed Set X for better config. Updated Balancer

--- src/main/kotlin/frc/robot/commands/Balancer.kt
+++ src/main/kotlin/frc/robot/commands/Balancer.kt
@@ -1,29 +1,40 @@
 package frc.robot.commands
 import edu.wpi.first.wpilibj.*
 import frc.robot.subsystems.SwerveSubsystem
-import com.kauailabs.navx.frc.AHRS
+import edu.wpi.first.wpilibj2.command.CommandBase
 
-class Balancer(
-    private val gyro: AHRS,
-    private val swerveDrive: SwerveSubsystem
-) {
-    private val toleranceDegrees = 1.0
-    private val targetAngle = 0.0
-    private val balanceSpeed = 0.2
+class Balancer(val swerveDrive: SwerveSubsystem): CommandBase() {
+    var gyro = swerveDrive.gyro
+    var pitch = gyro.pitch.toDouble()
+    val toleranceDegrees = 2.0//on the field, it is 2.5, but we dont want it getting too close
+    val targetAngle = 0.0
+    //This balance speed means that at 45 degrees off, the swerve will use 3.0 m/s to fix the error.
+    val balanceSpeed = 0.066 //TODO: TUNE
 
-    fun balance() {
-        while (!isBalanced()) {
-            val gyroAngle = gyro.angle
-            val error = targetAngle - gyroAngle
-            val correction = error * balanceSpeed
+    init {
+        addRequirements(swerveDrive)
+    }
+
+    override fun execute() {
+        pitch = gyro.pitch.toDouble()
+        if (!isBalanced()) {
+            var gyroAngle = pitch
+            var error = targetAngle - gyroAngle
+            var correction = error * balanceSpeed
             swerveDrive.drive(correction, 0.0, 0.0, false, true)
             Timer.delay(0.01)
+        } else {
+            swerveDrive.setX()
         }
+    }
+
+    fun isBalanced(): Boolean {
+        val gyroAngle = pitch
+        return gyroAngle in (targetAngle - toleranceDegrees)..(targetAngle + toleranceDegrees)
+    }
+
+    override fun end(interrupted: Boolean) {
         swerveDrive.setX()
     }
 
-    private fun isBalanced(): Boolean {
-        val gyroAngle = gyro.angle
-        return gyroAngle in (targetAngle - toleranceDegrees)..(targetAngle + toleranceDegrees)
-    }
 }
